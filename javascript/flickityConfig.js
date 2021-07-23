222ca8a1cc5d62546e49a75c6f8b4c119315932f
frkam <gearboxop@gmail.com>
2021-07-23 07:32:15 +0300

    add hamburger navigation

--- javascript/flickityConfig.js
+++ javascript/flickityConfig.js
@@ -10,3 +10,13 @@ const carouselFree = new Flickity(carouselFreeEl, {
   pageDots: false,
   autoPlay: true,
 });
+
+const navbar = document.querySelector(".navigation__link-box");
+
+const addResponsive = function () {
+  if (navbar.className === "navigation__link-box") {
+    navbar.className += " responsive";
+  } else {
+    navbar.className = "navigation__link-box";
+  }
+};
